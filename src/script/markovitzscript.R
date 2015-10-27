add0fcf7832da2640c028730802d65f106c477b2
SobieS93 <ssobiepanek@gmail.com>
2015-10-28 00:34:32 +0100

    r script updated

--- src/script/markovitzscript.R
+++ src/script/markovitzscript.R
@@ -1,71 +1,106 @@
-markovitz<-function(){
-# Map your working directory using setwd()
-# Read the relevant file
+# markowitzTest <-function(param,mode){
+# 	r1 <- read.csv("Markowitz.csv")
+# 	markowitz(r1,param,mode)
+# }
+# r1 is matrix of stock prices, param is number form 1-100 set by user, mode is chosen mode (optimal, given risk, given return)
 
-r1 <- read.csv("Markowitz.csv")
+markowitz<-function(r1,param,mode){
 
-
-mu = colMeans(r1[,-7])  ## Calculate the column means
-bigsig = cov(r1[,-7]) ## Variance-co-variance matrix
-
-
-m = nrow(bigsig)-1
-w = diff(c(0,sort(runif(m)), 1)); ## Assigning random weights between 0-1 to "w" which will have the dimension = no. of securities between which you have to divide your investment.
+	n = ncol(r1)
+	mu = colMeans(r1[,-n])  ## Calculate the column means 
+	bigsig = cov(r1[,-n]) ## Variance-co-variance matrix
 
 
-rb = sum(w*mu); ## Creating matrix "rb" which stores the E(r)
+	m = nrow(bigsig)-1
+	w = diff(c(0,sort(runif(m)), 1)); ## Assigning random weights between 0-1 to "w" which will have the dimension = no. of securities between which you have to divide your investment.
 
 
-sb = sum(w*bigsig*w); ## Creating matrix "sb" which stores sigma's
+	rb = sum(w*mu); ## Creating matrix "rb" which stores the E(r)
 
-max = (1000);
-wages = matrix( nrow = 1000, ncol = 7)
+	sb = sum(w*bigsig*w); ## Creating matrix "sb" which stores sigma's
 
-for (j in 1:1000) {
-    max=rbind(max,0);
+	max = (1000);
+	min = (1000);
+
+	wages = matrix( nrow = 1000, ncol = n)
+	
+	wagesMax = matrix( nrow = 1000, ncol = n)
+	wagesMin = matrix( nrow = 1000, ncol = n)
+
+	for (j in 1:1000) {
+		max=rbind(max,0);
+		min=rbind(min,1000);
+	}
+
+	N = 20000  ## Number of different combinations of "w" you want to look at
+
+
+	## Simulating the different combinations of weights "w's" and storing the E(r) and sigma^2
+	optswartz=-100;
+	optr=0;
+	opts=0;
+
+	for (j in 1:N) {
+		w = diff(c(0,sort(runif(m)), 1));
+		r = sum(w*mu); 
+		rb = rbind(rb,r); 
+		s = sum(w*bigsig*w); 
+		sb = rbind(sb,s); ## Note this is sigma^2 (variance)
+		k = floor(sqrt(s)*100);
+		if(r>max[k]){
+			max[k]=r;
+			for(m in 1:n){
+				wagesMax[k,m]=w[m];
+			}
+		}
+		rIndex = floor (r*1000) ;
+		#return (rIndex)
+		if(k<min[rIndex]){
+			min[rIndex]=k;
+			for(m in 1:n){
+				wagesMin[rIndex,m]=w[m];
+			}
+		}
+		if((r/k)>optswartz){
+			optswartz =r/s;
+			optr =r;
+			opts =s;
+			schwartzWages = w;
+		}
+	}
+	k = 1;
+	while(is.na(wagesMax[k,1])){
+		k=k+1;
+	}
+	minK = k;
+	while(k<1000){
+		if(is.na(wagesMax[k,1])){k=k+1;next;}
+		maxK = k;
+		k=k+1;
+	}
+	r = 1;
+	while(is.na(wagesMin[r,1])){
+		r=r+1;
+	}
+	minR = r;
+	while(r<1000){
+		if(is.na(wagesMin[r,1])){r=r+1;next;}
+		maxR= r;
+		r=r+1;
+	}
+	if(mode == 0){ 
+		ret = rbind(schwartzWages,optr);
+		ret = rbind(ret,opts);
+		return (ret);
+	}
+	if(mode == 1){
+		# param beetwen 1 to 100
+		ret = rbind(wagesMax [floor((maxK-minK)*param/100)+minK,],max[floor((maxK-minK)*param/100)])
+		return (ret)
+	}
+	if(mode == 2){
+		# param beetwen 1 to 100
+		ret = rbind(wagesMin [floor((maxR-minR)*param/100)+minR,],min[floor((maxR-minR)*param/100)])
+		return (ret)
+	}
 }
-
-N = 50000  ## Number of different combinations of "w" you want to look at
-
-
-## Simulating the different combinations of weights "w's" and storing the E(r) and sigma^2
-
-for (j in 2:N) {
-w = diff(c(0,sort(runif(m)), 1));
-r = sum(w*mu); rb = rbind(rb,r);
-s = sum(w*bigsig*w); sb = rbind(sb,s); ## Note this is sigma^2 (variance)
-k = floor(sqrt(s)*100);
-if(r>max[k]){
-    max[k][1]=r[1];
-    wages[k,1]=w[1];
-    wages[k,2]=w[2];
-    wages[k,3]=w[3];
-    wages[k,4]=w[4];
-    wages[k,5]=w[5];
-    wages[k,6]=w[6];
-    wages[k,7]=r;
-}
-}
-k = 1;
-while(is.na(wages[k,1])){
-k=k+1;
-}
-ret=wages[k,];
-ret=rbind(ret,ret)
-k=k+1;
-while(k<1000){
-    if(is.na(wages[k,7])){k=k+1;next;}
-    size=nrow(ret);
-    if(ret[size,7]<wages[k,7]){
-        ret=rbind(ret,wages[k,]);
-    }
-    k=k+1;
-}
-return (ret);
-}
-d = data.frame(rb, sb); ## Merge all the E(r) and sigmas in one data.frame.
-
-d$sb = sqrt(d$sb); ## Square root the variance to get the sigmas
-
-plot(d$sb, d$rb, ylab="E(r)", xlab="Sigma", col="blue",  xlim = c(0.5,10), ylim = c(-0.1,0.5), main = "E(r)- sigma (With risk free asset), N = 200000")
-
