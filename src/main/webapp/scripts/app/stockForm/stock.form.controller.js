0900bedd9f6dedbb14395d1f4293ace96e3af95b
rasgrass <maniu611@gmail.com>
2015-12-01 23:57:58 +0100

    Added Stock wallet form

--- src/main/webapp/scripts/app/stockForm/stock.form.controller.js
+++ src/main/webapp/scripts/app/stockForm/stock.form.controller.js
@@ -1,9 +1,8 @@
 'use strict';
 
 angular.module('cabzzaApp')
-		.controller('StockFormController',function ($scope, $state, $rootScope) {
+		.controller('StockFormController', function ($scope, $state, walletFormService, StockWallet, DateUtils, Account) {
 			$scope.datepickerOptions = {
-				format: 'dd/MM/yyyy',
 				language: 'pl',
 				autoclose: true,
 				weekStart: 0
@@ -12,14 +11,28 @@ angular.module('cabzzaApp')
 				if ($state.current.name === 'step1') {
 					$state.go('step2');
 				} else if ($state.current.name === 'step2') {
+					walletFormService.wallet.walletName = $scope.name;
 					$state.go('step3');
 				} else if ($state.current.name === 'step3') {
-					$rootScope.$broadcast('sliders');
+					walletFormService.wallet.quoteSymbols = 'MSFT';
 					$state.go('step4');
 				} else if ($state.current.name === 'step4') {
+					walletFormService.wallet.profit = $scope.profit;
+					walletFormService.wallet.security = $scope.security;
+					walletFormService.wallet.optimalWallet = $scope.optimalWallet;
 					$state.go('step5');
 				} else if ($state.current.name === 'step5') {
-					$state.go('step6');
+					walletFormService.wallet.startDate = DateUtils.convertLocaleDateToServer(new Date($scope.startDate));
+					walletFormService.wallet.timeMiddle = $scope.middleDate;
+					walletFormService.wallet.endDate = DateUtils.convertLocaleDateToServer(new Date($scope.endDate));
+					var id;
+					Account.get().$promise
+							.then(function (account) {
+								id = account.data.id;
+								walletFormService.wallet.ownerId = id;
+								StockWallet.save(walletFormService.wallet);
+								$state.go('step6');
+							});
 				} else if ($state.current.name === 'step6') {
 					$state.go('home');
 				}
